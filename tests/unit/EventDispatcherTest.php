615d6a67d81e55765545b3f8647322f86454545c
Juriy Panasevich <elfelrandcor@gmail.com>
2014-05-30 13:40:11 +0400

    EventDispatcherTest

--- tests/unit/EventDispatcherTest.php
+++ tests/unit/EventDispatcherTest.php
@@ -1,6 +1,35 @@
 <?php
 
-class EventDispatcherTest {
+use WS\EventDispatcher\Event;
+use WS\EventDispatcher\EventDispatcher;
+use WS\EventDispatcher\Handler;
 
+class EventDispatcherTest extends CTestCase {
+
+    public function testAttachDetachHandler() {
+        $dispatcher = new EventDispatcher();
+        $this->assertNull($dispatcher->events);
+        $dispatcher->attachHandler(TestEvent::className(), TestHandler::className(), array(
+            'testParan' => 1
+        ));
+        $this->assertEquals(1, count($dispatcher->events));
+
+        $dispatcher->detachHandler(TestHandler::className(), TestEvent::className());
+        $this->assertNull(count($dispatcher->events));
+    }
 }
- 
+
+
+class TestEvent extends Event {
+
+    public function attributeNames() {
+
+    }
+}
+
+class TestHandler extends Handler {
+
+    protected function process() {
+
+    }
+}
